837b159adc3847b318d1c2798ff20afe18f64f74
LDJ <605202637@qq.com>
2018-06-28 18:40:07 +0800

    增加了搜索接口，使用RecyclerView做界面

--- Contacts/app/src/main/java/com/ldj/wow/contacts/MainActivity.java
+++ Contacts/app/src/main/java/com/ldj/wow/contacts/MainActivity.java
@@ -1,33 +1,45 @@
 package com.ldj.wow.contacts;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import cc.solart.wave.WaveSideBarView;
 public class MainActivity extends AppCompatActivity {
     public WaveSideBarView mWave;
+    public RecyclerView mRecView;
+    public SearchView mSearchView;
+    public TextView mSearchText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        mWave = (WaveSideBarView) findViewById(R.id.side_view);
-        /*mWave.setOnTouchLetterChangeListener(new WaveSideBarView.OnTouchLetterChangeListener() {
-            @Override
-            public void onLetterChange(String letter) {
-                int pos = adapter.getLetterPosition(letter);
 
-                if (pos != -1) {
-                    mRecyclerView.scrollToPosition(pos);
-                }
-            }
-        });*/
+        List<String> data = new ArrayList();
+        data.add("J黎丁嘉");
+        data.add("L梁沛霖");
+        data.add("Z梁志聪 ");
+        mWave = (WaveSideBarView) findViewById(R.id.side_view);
+        mRecView = (RecyclerView) findViewById(R.id.recView);
+        mRecView.setLayoutManager(new LinearLayoutManager(this));
+        mRecView.setAdapter(new NormalAdapter(data));
+        mSearchText = (TextView) findViewById(R.id.search_Text);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +54,35 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        mSearchView = (SearchView) findViewById(R.id.searchView);
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (!TextUtils.isEmpty(query)){
+                    Toast toast=Toast.makeText(getApplicationContext(), query, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else {
+                    Toast toast=Toast.makeText(getApplicationContext(), "空搜索字符串", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (mSearchView != null){
+                    // 得到输入管理对象
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if (imm != null) {
+                        // 这将让键盘在所有的情况下都被隐藏，但是一般我们在点击搜索按钮后，输入法都会乖乖的自动隐藏的。
+                        imm.hideSoftInputFromWindow(mSearchView.getWindowToken(), 0); // 输入法如果是显示状态，那么就隐藏输入法
+                    }
+                    mSearchView.clearFocus(); // 不获取焦点
+                }
+                return false;
+            }
+        });
         return true;
     }
 
