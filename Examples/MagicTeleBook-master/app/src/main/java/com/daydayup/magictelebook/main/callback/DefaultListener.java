527ae52f663bf5f00f942d574c3ff8a84b45186a
LDJ <605202637@qq.com>
2018-06-30 19:19:12 +0800

    字母导航栏功能上线

--- Examples/MagicTeleBook-master/app/src/main/java/com/daydayup/magictelebook/main/callback/DefaultListener.java
+++
@@ -1,9 +0,0 @@
-package com.daydayup.magictelebook.main.callback;
-
-/**
- * Created by Jay on 16/5/25.
- */
-public interface DefaultListener {
-    void onSuccess(String msg);
-    void onFailed(String msg);
-}
--- Examples/MagicTeleBook-master/app/src/main/java/com/daydayup/magictelebook/main/callback/IContactViewHolderClicks.java
+++
@@ -1,8 +0,0 @@
-package com.daydayup.magictelebook.main.callback;
-
-/**
- * Created by Jallen on 2016/5/10.
- */
-public interface IContactViewHolderClicks {
-    public void onItemClick();
-}
