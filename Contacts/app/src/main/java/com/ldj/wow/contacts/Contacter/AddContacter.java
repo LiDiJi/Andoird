59c8c83d1e7c6098f5a32628d59c64d2bab94fa8
LDJ <605202637@qq.com>
2018-07-02 23:24:46 +0800

    修正了新增联系人界面，输入数据为空时报错的bug

--- Contacts/app/src/main/java/com/ldj/wow/contacts/Contacter/AddContacter.java
+++ Contacts/app/src/main/java/com/ldj/wow/contacts/Contacter/AddContacter.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,26 +21,42 @@ import com.ldj.wow.contacts.R;
 public class AddContacter extends Activity {
     private Button Confirm_btn;
     private Button Cancal_btn;
+    EditText nameText, phoneText, emailText, organizationText;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_contacter);
         StatusBarCompat.setStatusBarColor(this, Color.parseColor("#FFFFFF"),true);
         Confirm_btn = (Button) findViewById(R.id.confirm_new);
         Cancal_btn = (Button) findViewById(R.id.cancal_new);
+        nameText = (EditText) findViewById(R.id.username);
+        phoneText = (EditText) findViewById(R.id.userphone);
+        emailText = (EditText) findViewById(R.id.useremail);
+        organizationText = (EditText) findViewById(R.id.userorganization);
 
         Confirm_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText nameText, phoneText, emailText, organizationText;
                 String name, phone, email, organization;
-                nameText = (EditText) findViewById(R.id.username);
-                name = nameText.getText().toString();
-                phoneText = (EditText) findViewById(R.id.userphone);
-                phone = phoneText.getText().toString();
-                emailText = (EditText) findViewById(R.id.useremail);
-                email = emailText.getText().toString();
-                organizationText = (EditText) findViewById(R.id.userorganization);
-                organization = organizationText.getText().toString();
+                if (TextUtils.isEmpty(nameText.getText())){
+                    name =  "联系人";
+                }
+                else name = nameText.getText().toString();
+
+                if (TextUtils.isEmpty(phoneText.getText())){
+                    phone = "110";
+                }
+                else phone = phoneText.getText().toString();
+
+                if (TextUtils.isEmpty(emailText.getText())){
+                    email = "@email.com";
+                }
+                else email = emailText.getText().toString();
+
+                if (TextUtils.isEmpty(organizationText.getText())){
+                    organization = "SYSU";
+                }
+                else organization = organizationText.getText().toString();
+
                 ContactModel sample = new ContactModel(name, phone, email, organization);
             }
         });
